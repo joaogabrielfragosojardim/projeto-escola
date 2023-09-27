90a05a2f719336fd6ebf0f576b1936c05452e17d
Pedro Miguel <pedromrap@gmail.com>
2023-09-27 10:48:45 -0300

    feat: add permissions middleware in user and auth routes

--- src/pages/api/auth/index.ts
+++ src/pages/api/auth/index.ts
@@ -1,18 +1,38 @@
-import { RouteHandler } from '@/components/api/RouteHandler';
-import { authenticate } from '@/controllers/user/authenticate';
+import { HttpMethod, RouteHandler } from '@/components/api/RouteHandler';
+import { AuthenticateController } from '@/controllers/auth';
+import { Role } from '@/types/roles';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
+
+const authMethods: Record<HttpMethod, boolean> = {
+  GET: false,
+  POST: false,
+  DELETE: false,
+  PUT: false,
+};
+
+const permissionMethods: Record<HttpMethod, Role[]> = {
+  GET: [],
+  POST: [],
+  PUT: [],
+  DELETE: [],
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const method = req.method as HttpMethod;
+
+  const authenticateController = new AuthenticateController();
+
   await RouteHandler(
     req,
     res,
     {
-      POST: authenticate,
+      POST: authenticateController.handle,
     },
-    false
+    authMethods[method],
+    permissionMethods[method]
   );
 }
