7aed8f927b83dba3a317d929cb2cf3be083b8336
Pedro Miguel <pedromrap@gmail.com>
2023-09-27 10:42:52 -0300

    feat: add school and project table

---
+++ prisma/migrations/20230922133644_create_school_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "School" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+
+    CONSTRAINT "School_pkey" PRIMARY KEY ("id")
+);
