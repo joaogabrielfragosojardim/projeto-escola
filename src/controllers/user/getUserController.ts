38816e2719c93e403337267e87e30d80c59ea508
Pedro Miguel <pedromrap@gmail.com>
2023-10-11 11:59:16 -0300

    feat: create and edit user endpoints

---
+++ src/controllers/user/getUserController.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next/types';
+
+import { GetUserUseCase } from '@/useCases/getUserUserUseCase';
+
+
+export class GetUserController {
+  async handle(req: NextApiRequest, res: NextApiResponse) {
+    try {
+      const { userId } = req;
+
+      const getUserUseCase = new GetUserUseCase();
+
+      const user = await getUserUseCase.execute({
+        id: userId,
+      });
+
+      return res.status(201).json(user);
+    } catch (error) {
+      console.log(error);
+
+      throw error;
+    }
+  }
+}
--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -3,12 +3,14 @@ import type { NextApiRequest, NextApiResponse } from 'next/types';
 import type { HttpMethod } from '@/components/api/RouteHandler';
 import { RouteHandler } from '@/components/api/RouteHandler';
 import { CreateUserController } from '@/controllers/user';
+import { EditUserController } from '@/controllers/user/editUserController';
+import { GetUserController } from '@/controllers/user/getUserController';
 
 const authMethods: Record<HttpMethod, boolean> = {
-  GET: false,
+  GET: true,
   POST: true,
   DELETE: false,
-  PUT: false,
+  PUT: true,
 };
 
 export default async function handler(
@@ -18,12 +20,16 @@ export default async function handler(
   const method = req.method as HttpMethod;
 
   const createUserController = new CreateUserController();
+  const getUserController = new GetUserController();
+  const editUserController = new EditUserController();
 
   await RouteHandler(
     req,
     res,
     {
       POST: createUserController.handle,
+      GET: getUserController.handle,
+      PUT: editUserController.handle,
     },
     authMethods[method],
   );
