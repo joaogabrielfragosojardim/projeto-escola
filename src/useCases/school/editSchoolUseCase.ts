fabf6444f703cf4cf0db479055f0a236bd50b0b1
Pedro Miguel <pedromrap@gmail.com>
2023-10-15 18:07:39 -0300

    feat: update edit school endpoint

--- src/useCases/school/editSchoolUseCase.ts
+++ src/useCases/school/editSchoolUseCase.ts
@@ -1,33 +1,43 @@
 import { prisma } from '@/lib/prisma';
 
 interface EditSchoolUseCaseRequest {
-  id: string | undefined;
+  id?: string;
   name: string;
-  city: string;
-  state: string;
   projectId: string;
+  address: {
+    zipCode: string;
+    city: string;
+    state: string;
+    street: string;
+  };
 }
 
 export class EditSchoolUseCase {
-  async execute({
-    id,
-    name,
-    city,
-    state,
-    projectId,
-  }: EditSchoolUseCaseRequest) {
+  async execute({ id, name, projectId, address }: EditSchoolUseCaseRequest) {
     const school = await prisma.school.update({
       where: { id },
       data: {
         name,
-        city,
-        state,
         projectId,
       },
     });
 
+    const newAddress = await prisma.address.update({
+      where: { id: school.addressId },
+      data: {
+        city: address.city,
+        state: address.state,
+        zipCode: address.zipCode,
+        street: address.street,
+      },
+    });
+
     return {
-      school,
+      school: {
+        name: school.name,
+        projectId: school.projectId,
+        address: newAddress,
+      },
     };
   }
 }
