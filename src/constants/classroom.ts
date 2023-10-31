bfcd2c428ac9c287c7582d35f924bcba91dda652
Pedro Miguel <pedromrap@gmail.com>
2023-10-31 10:38:56 -0300

    chore: add classroom options

--- src/constants/classroom.ts
+++ src/constants/classroom.ts
@@ -1,3 +1,21 @@
 export const allSeries = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export const allPeriods = ['Matutino', 'Vespertino', 'Noturno'];
+
+export type ClassRoom = {
+  label: string;
+  value: {
+    series: number;
+    period: string;
+  };
+};
+
+export const classrooms: ClassRoom[] = allSeries.flatMap((series) =>
+  allPeriods.map((period) => ({
+    label: `${series}º ano - ${period}`,
+    value: {
+      series,
+      period,
+    },
+  })),
+);
