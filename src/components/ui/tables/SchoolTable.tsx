4331a62a2935922febba1133865a9948191a1ccc
joaogabriel.dsn.cir <joaogabriel.dsn.cir@alterdata.com.br>
2024-01-10 21:26:48 -0300

    fix(table quantity): table quantity

--- src/components/ui/tables/SchoolTable.tsx
+++ src/components/ui/tables/SchoolTable.tsx
@@ -193,7 +193,7 @@ export const SchoolTable = ({
 
   useEffect(() => {
     refetch();
-  }, [filtersValues, page, refetch]);
+  }, [filtersValues, page, perPage, refetch]);
 
   useEffect(() => {
     setTotalPages(data?.meta.totalPage);
