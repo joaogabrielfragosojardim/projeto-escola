b8f9d23bebfba40c55930fd5067a0c010c085d7f
joaogabriel.dsn.cir <joaogabriel.dsn.cir@alterdata.com.br>
2024-01-17 18:07:42 -0300

    fix(fix name): fix name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Books } from '@/components/ui/site/Books';
 import { Feedbacks } from '@/components/ui/site/Feedbacks';
 import { Footer } from '@/components/ui/site/Footer';
 import { Header } from '@/components/ui/site/Header';
-import { Hero } from '@/components/ui/site/s';
+import { Hero } from '@/components/ui/site/Hero';
 import { HoWeAre } from '@/components/ui/site/HoWeAre';
 import { OurProjects } from '@/components/ui/site/OurProjects';
 
