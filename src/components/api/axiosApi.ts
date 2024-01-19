e21600ac76fbdbe296ffd3b2c92d6ba10dbe46bb
Escolaprime <escolaprimeadm@gmail.com>
2024-01-19 10:07:01 -0300

    feat(cors in axios): cors in axios

--- src/components/api/axiosApi.ts
+++ src/components/api/axiosApi.ts
@@ -3,6 +3,9 @@ import Axios from 'axios';
 export const axiosApi = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Authorization',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
