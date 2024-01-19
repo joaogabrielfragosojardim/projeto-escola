6721d56e1fadb31e214e81dcbc7b1dc50e6ff58d
Escolaprime <escolaprimeadm@gmail.com>
2024-01-19 09:40:22 -0300

    fix(fix cors): fix cors

--- next.config.mjs
+++ next.config.mjs
@@ -39,4 +39,29 @@ export default bundleAnalyzer({
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value:
+              'https://escolaprimepe.com.br, https://www.escolaprimepe.com.br',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 });
